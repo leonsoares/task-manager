ef8e05ab3e26dc98dc61e3ef34254475d846000b
LeonSoares <leonsoares3@gmail.com>
2021-03-20 12:14:23 -0400

    update mock up version of app with json server

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
 
-const Header = (props) => {
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+  
     return (
-        <header>
-            <h1>Lets built our app</h1>
-            <p>
-                {props.test}
-            </p>
-        </header>
+      <header className='header'>
+        <h1>{title}</h1>
+        {location.pathname === '/' && (
+          <Button
+            color={showAdd ? 'red' : 'green'}
+            text={showAdd ? 'Close' : 'Add'}
+            onClick={onAdd}
+          />
+        )}
+      </header>
     )
+  }
+
+Header.defaultProps = {
+title: 'Task Manager',
+}
+
+Header.propTypes = {
+title: PropTypes.string.isRequired,
 }
 
 export default Header
